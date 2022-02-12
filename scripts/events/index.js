a522d0735ce49aca84c0a2483ce0a325aac42c75
szyink <me@szyink.com>
2022-02-12 15:19:21 +0800

    test

--- scripts/events/index.js
+++ scripts/events/index.js
@@ -8,3 +8,19 @@ hexo.on('generateBefore', () => {
   require('./lib/utils')(hexo);
   require('./lib/renderStylus')(hexo);
 });
+
+const chalk = require('chalk')
+hexo.extend.filter.register('template_locals', function (locals) {
+  const all_posts = locals.site.all_posts
+
+  if (!all_posts) {
+    hexo.log.debug('failed to get all_posts, is hexo-hide-posts enabled?')
+    return
+  }
+
+  locals.site.posts = locals.site.all_posts
+  hexo.log.debug('current page is ' + chalk.magenta(locals.path))
+  hexo.log.debug('set site.posts to all_posts, length is ' + all_posts.length)
+
+  return locals
+})
